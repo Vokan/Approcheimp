bc094748a6684f9949f74fd58581d488ef4566f8
Vokan Kocak <vokankocak@gmail.com>
2020-06-05 20:23:39 +0200

    TP3

--- src/fr/declaration/DeclarationApp.java
+++ src/fr/declaration/DeclarationApp.java
@@ -4,8 +4,16 @@ public class DeclarationApp {
 
 	public static void main(String[] arg) {
 		byte a = 7;
+		short b = 17;
+		int c = 56;
+		long d = 6666666;
+		float e = 1.33F;
+		double f = 8.8;
+		char g = 'm';
+		boolean h = false;
 		
-		
-	}
-
+	System.out.println(a);
+		}
+	String randomString = "voici le résultat d'un calcule 1+5 = 6";
+	
 }
